e3f35fd88653ca6eedb77b26697cccd943d37da6
Thomas Munguya <thomas@thezig.io>
2022-08-13 09:46:35 +0200

    Initial commit.

---
+++ src/main/java/com/thescholl/ssiach6ex1/SpringSecurityInActionCh6Exercise1Application.java
@@ -0,0 +1,13 @@
+package com.thescholl.ssiach6ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInActionCh6Exercise1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityInActionCh6Exercise1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/thescholl/ssiach6ex1/SpringSecurityInActionCh6Exercise1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thescholl.ssiach6ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInActionCh6Exercise1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
